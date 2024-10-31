a185d9f67b4424a8cbfb61b1c6ba0f285d4a4a70
unknown <kamenko.gennady@gmail.com>
2024-10-31 23:01:51 +0300

    add eslint-plugin-prettier, and default eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,75 @@
+// const nx = require('@nx/eslint-plugin');
+// const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+// module.exports = [
+//   ...nx.configs['flat/base'],
+//   ...nx.configs['flat/typescript'],
+//   ...nx.configs['flat/javascript'],
+//   {
+//     ignores: ['**/dist'],
+//   },
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+//     rules: {},
+//   },
+//   ...nx.configs['flat/angular'],
+//   ...nx.configs['flat/angular-template'],
+//   {
+//     files: ['**/*.ts', '**/*.html'],
+//     rules: {
+//       'no-unused-vars': 'warn',
+//       '@typescript-eslint/no-unused-expressions': ['error', {'allowTernary' : true }],
+//       'object-shorthand': 'error',
+//       'curly': 'error',
+//       'semi': 'error',
+//       'no-redeclare': 'error',
+//       'prefer-const': 'error',
+//       'quotes': ['error', 'single'],
+//       'eqeqeq': 'error',
+//       'no-unreachable': 'error',
+//       'keyword-spacing': ['error', { 'after': true }],
+//       '@angular-eslint/directive-selector': [
+//         'error',
+//         {
+//           type: 'attribute',
+//           prefix: 'app',
+//           style: 'camelCase',
+//         },
+//       ],
+//       '@angular-eslint/component-selector': [
+//         'error',
+//         {
+//           type: 'element',
+//           prefix: 'app',
+//           style: 'kebab-case',
+//         },
+//       ],
+//     },
+//   },
+// ];
+
+
+
+
+
 const nx = require('@nx/eslint-plugin');
-const eslintConfigPrettier = require( "eslint-config-prettier");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
 
 module.exports = [
-  eslintConfigPrettier,
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
   {
     ignores: ['**/dist'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
     rules: {},
   },
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
   {
-    files: ['**/*.ts', '**/*.html'],
+    files: ['**/*.ts'],
     rules: {
-      'no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-expressions': ['error', {'allowTernary' : true }],
-      'object-shorthand': 'error',
-      'curly': 'error',
-      'semi': 'error',
-      'no-redeclare': 'error',
-      'prefer-const': 'error',
-      'quotes': ['error', 'single'],
-      'eqeqeq': 'error',
-      'no-unreachable': 'error',
-      'keyword-spacing': ['error', { 'after': true }],
       '@angular-eslint/directive-selector': [
         'error',
         {
@@ -47,4 +88,10 @@ module.exports = [
       ],
     },
   },
+  {
+    files: ['**/*.html'],
+    // Override or add rules here
+    rules: {},
+  },
 ];
+
