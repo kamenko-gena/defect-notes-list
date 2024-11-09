ae882514bce74c79cfaf493ba1ccc03a1fbb5b4c
unknown <kamenko.gennady@gmail.com>
2024-11-09 23:01:11 +0300

    add navigation in my-notes-page and my-notes-create-page

--- src/app/components/pages/my-notes-page/my-notes-create-page/my-notes-create-page.component.ts
+++ src/app/components/pages/my-notes-page/my-notes-create-page/my-notes-create-page.component.ts
@@ -1,7 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TuiAlertModule, TuiDialogModule, TuiRootModule } from '@taiga-ui/core';
+import {
+    TuiAlertModule,
+    TuiDialogModule,
+    TuiLinkModule,
+    TuiRootModule,
+} from '@taiga-ui/core';
 import { CreateNoteFormComponent } from 'src/app/components/create-note-form/create-note-form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-my-notes-create-page',
@@ -12,6 +18,8 @@ import { CreateNoteFormComponent } from 'src/app/components/create-note-form/cre
         TuiDialogModule,
         TuiAlertModule,
         CreateNoteFormComponent,
+        RouterLink,
+        TuiLinkModule,
     ],
     templateUrl: './my-notes-create-page.component.html',
     styleUrl: './my-notes-create-page.component.less',
--- src/app/components/pages/my-notes-page/my-notes-page.component.ts
+++ src/app/components/pages/my-notes-page/my-notes-page.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotesListComponent } from '../../notes-list/notes-list.component';
+import { TuiAlertModule, TuiLinkModule, TuiRootModule } from '@taiga-ui/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-my-notes-page',
     standalone: true,
-    imports: [CommonModule, NotesListComponent],
+    imports: [
+        CommonModule,
+        NotesListComponent,
+        TuiRootModule,
+        RouterLink,
+        TuiLinkModule,
+        TuiAlertModule,
+    ],
     templateUrl: './my-notes-page.component.html',
     styleUrl: './my-notes-page.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
