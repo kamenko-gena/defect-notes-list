836b2be3870bcc48b638de1e2bd554d5d9aa7da9
unknown <kamenko.gennady@gmail.com>
2024-11-11 00:18:45 +0300

    applied prettier

--- src/app/resolvers/note-id.resolver.spec.ts
+++ src/app/resolvers/note-id.resolver.spec.ts
@@ -4,14 +4,16 @@ import { ResolveFn } from '@angular/router';
 import { noteIdResolver } from './note-id.resolver';
 
 describe('noteIdResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
-      TestBed.runInInjectionContext(() => noteIdResolver(...resolverParameters));
+    const executeResolver: ResolveFn<boolean> = (...resolverParameters) =>
+        TestBed.runInInjectionContext(() =>
+            noteIdResolver(...resolverParameters),
+        );
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+    });
 
-  it('should be created', () => {
-    expect(executeResolver).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(executeResolver).toBeTruthy();
+    });
 });
--- src/app/services/firebase-storage-service/firebase-storage.service.spec.ts
+++ src/app/services/firebase-storage-service/firebase-storage.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { FirebaseStorageService } from './firebase-storage.service';
 
 describe('FirebaseStorageService', () => {
-  let service: FirebaseStorageService;
+    let service: FirebaseStorageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FirebaseStorageService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(FirebaseStorageService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
