90bf1ebfafa61c2fe1698efb3c488eb89f1b46a7
unknown <kamenko.gennady@gmail.com>
2024-11-10 23:12:53 +0300

    change name of formControl from solutionCheckeControl to isCompleted

--- src/app/directives/solution-descript-validator/solution-descript-validator.directive.ts
+++ src/app/directives/solution-descript-validator/solution-descript-validator.directive.ts
@@ -6,20 +6,18 @@ import { AbstractControl, FormControl, Validators } from '@angular/forms';
     standalone: true,
 })
 export class SolutionDescriptValidatorDirective implements OnInit {
-    @Input({ required: true }) solutionCheckControl!: AbstractControl;
+    @Input({ required: true }) isCompletedControl!: AbstractControl;
     @Input({ required: true }) solutionDescriptControl!: FormControl;
 
     ngOnInit() {
-        this.solutionCheckControl.valueChanges.subscribe(
-            (isChecked: boolean) => {
-                isChecked
-                    ? this.solutionDescriptControl.setValidators([
-                          Validators.required,
-                          Validators.maxLength(500),
-                      ])
-                    : this.solutionDescriptControl.clearValidators();
-                this.solutionDescriptControl.updateValueAndValidity();
-            },
-        );
+        this.isCompletedControl.valueChanges.subscribe((isChecked: boolean) => {
+            isChecked
+                ? this.solutionDescriptControl.setValidators([
+                      Validators.required,
+                      Validators.maxLength(500),
+                  ])
+                : this.solutionDescriptControl.clearValidators();
+            this.solutionDescriptControl.updateValueAndValidity();
+        });
     }
 }
