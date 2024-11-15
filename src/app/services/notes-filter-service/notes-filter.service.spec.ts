ebbf74077952fad825b1ff02b7f1f0cdd1ceb944
unknown <kamenko.gennady@gmail.com>
2024-11-15 14:08:10 +0300

    applied prettier

--- src/app/services/notes-filter-service/notes-filter.service.spec.ts
+++ src/app/services/notes-filter-service/notes-filter.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { NotesFilterService } from './notes-filter.service';
 
 describe('NotesFilterService', () => {
-  let service: NotesFilterService;
+    let service: NotesFilterService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NotesFilterService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(NotesFilterService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
