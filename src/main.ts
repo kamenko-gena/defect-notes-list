45d7396084e5cb853c21e398c1780bddcf4a9b94
unknown <kamenko.gennady@gmail.com>
2024-11-16 23:57:55 +0300

    add Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import * as Sentry from '@sentry/angular';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+Sentry.init({
+    dsn: 'https://fa2a8348e65c8948b3b1c17e711f05b0@o4508291284271104.ingest.de.sentry.io/4508291289514064',
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err),
