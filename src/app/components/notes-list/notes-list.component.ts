a73d8a1859acbba4a7afc77f1cc5adc1f91d9a2c
unknown <kamenko.gennady@gmail.com>
2024-11-19 12:34:53 +0300

    fixed notes-list-component, add addaptive style, try to use tui sort in table

--- src/app/components/notes-list/notes-list.component.ts
+++ src/app/components/notes-list/notes-list.component.ts
@@ -2,6 +2,7 @@ import {
     ChangeDetectionStrategy,
     Component,
     inject,
+    OnDestroy,
     OnInit,
     signal,
 } from '@angular/core';
@@ -9,7 +10,6 @@ import { CommonModule } from '@angular/common';
 import {
     TuiAvatarModule,
     TuiBadgeModule,
-    TuiDataListWrapperModule,
     TuiSelectModule,
 } from '@taiga-ui/kit';
 import {
@@ -20,13 +20,12 @@ import {
 import { FirebaseStorageService } from 'src/app/services/firebase-storage-service/firebase-storage.service';
 import { NoteInterface } from 'src/app/interfaces/note-interface';
 import { RouterLink } from '@angular/router';
-import { TuiTableModule } from '@taiga-ui/addon-table';
-import { map, take } from 'rxjs';
+import { TuiComparator, TuiTableModule } from '@taiga-ui/addon-table';
+import { map, Subscription, take } from 'rxjs';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NotesFilterService } from 'src/app/services/notes-filter-service/notes-filter.service';
 
 const FILTER_SECTION_NAME = [
-    'Все',
     'Пожарная автоматика',
     'Охранная сигнализация',
     'Управление доступом',
@@ -48,7 +47,6 @@ type Section = NoteSections[number];
         TuiAvatarModule,
         TuiTableModule,
         TuiSelectModule,
-        TuiDataListWrapperModule,
         TuiTextfieldControllerModule,
         FormsModule,
         ReactiveFormsModule,
@@ -58,27 +56,33 @@ type Section = NoteSections[number];
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [NotesFilterService],
 })
-export class NotesListComponent implements OnInit {
-    readonly tableColName = [
-        'Раздел',
-        'Название',
-        'Статус',
-        'Автор',
-        'Дата',
-        'Ссылка',
-    ];
+export class NotesListComponent implements OnInit, OnDestroy {
+    readonly tableColName = ['Название', 'Раздел', 'Статус', 'Автор', 'Дата'];
     private readonly firebaseStorageService = inject(FirebaseStorageService);
     private readonly notesFilterService = inject(NotesFilterService);
+    private subscription: Subscription = new Subscription();
     readonly filterName = FILTER_SECTION_NAME;
     private filterDataFlag = false;
     private filterComplitedFlag = false;
 
     readonly notesFromFirebase = signal<NoteInterface[]>([]);
 
-    readonly filterSectionName = new FormControl<Section>('Все');
+    readonly filterSectionName = new FormControl<Section | null>(null);
+
+    readonly totalSorter: TuiComparator<NoteInterface> = (a, b) =>
+        Number(a.isCompleted) - Number(b.isCompleted);
 
     ngOnInit(): void {
         this.getNotes();
+        this.subscription = this.filterSectionName.valueChanges.subscribe(
+            () => {
+                this.filterBySection();
+            },
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 
     private getNotes(): void {
@@ -116,7 +120,7 @@ export class NotesListComponent implements OnInit {
     }
 
     filterBySection(): void {
-        if (this.filterSectionName.value === 'Все') return this.getNotes();
+        if (!this.filterSectionName.value) return this.getNotes();
 
         this.firebaseStorageService
             .getNotes()
